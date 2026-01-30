002f1dfc2a0bc5900a89ed45f2d22ec01731f6ca
Phút Thịn <thinhtran0823@gmail.com>
2026-01-30 22:12:25 +0700

    First commit

--- app.js
+++ app.js
@@ -8,5 +8,25 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
+const contactsRouter = require("./app/routes/contact.route");
+module.exports = app;
+app.use("/api/contacts", contactsRouter);
+
+const ApiError = require("./app/api-error");
+// ... (code cũ)
+
+app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,3 +7,12 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
 ]);
+
+module.exports = {
+    env: {
+        node: true,      // Quan trọng: Báo cho ESLint biết đây là môi trường Node.js
+        commonjs: true,  // Quan trọng: Cho phép dùng require, module.exports
+        es2021: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
+};
